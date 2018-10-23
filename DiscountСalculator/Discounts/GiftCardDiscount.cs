fd92d370eebc54f75ba4278c32445f847cb77d9a
igorjouk <igrokk70@gmail.com>
2018-10-24 01:51:10 +0300

    introduce gift card class

---
+++ DiscountСalculator/Discounts/GiftCardDiscount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountСalculator.Discounts
+{
+    class GiftCardDiscount : IDiscount
+    {
+        public DateTime Validity { get; set; }
+        public double ApplyDiscount(double productPrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
