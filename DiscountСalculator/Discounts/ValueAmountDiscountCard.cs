2d6a5169343e003a622c4e0f00559532b33a577e
igorjouk <igrokk70@gmail.com>
2018-10-25 01:02:07 +0300

    added new amount discount card

---
+++ DiscountСalculator/Discounts/ValueAmountDiscountCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DiscountСalculator.Discounts
+{
+    class ValueAmountDiscountCard : IDiscount
+    {
+        private double _discount;
+
+        public ValueAmountDiscountCard(double discount)
+        {
+            _discount = discount;
+        }
+        public double ApplyDiscount(double productPrice)
+        {
+            return productPrice - _discount;
+        }
+    }
+}
